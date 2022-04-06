cc9a7bb797bdf7a7163d80249a8ccbc7005807d8
andre <andrecosta8@gmail.com>
2022-04-06 11:04:15 +0100

    add signup and login routes, express-session

--- models/User.model.js
+++ models/User.model.js
@@ -3,14 +3,51 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    firstName: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: true,
     },
-    password: String,
+    lastName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postcode: {
+      type: String,
+      required: true,
+    },
+    tax: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+    },
+
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
